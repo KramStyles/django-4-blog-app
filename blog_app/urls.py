f3276de16474490bc41e3ba596ac9cb416a9a7e6
kramstyles <mark.eke@decagon.dev>
2022-03-28 23:35:51 +0100

    added html and css files"

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog_home')
+    path('', views.index, name='blog_home'),
+    path('about/', views.about, name='blog_about'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('<h2>Welcome to My blog</h2>')
+
+
+def about(request):
+    return HttpResponse('<h2>Blog About page </h2>')
