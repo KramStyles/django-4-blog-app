4aa64405d513dd0e23b121ad2286e3bc707c5522
kramstyles <mark.eke@decagon.dev>
2022-03-29 19:02:54 +0100

    created register.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    context = {
+        'form': UserCreationForm()
+    }
+    return render(request, 'users/register.html', context)
