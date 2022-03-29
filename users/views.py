a1e64b7b3bc4ee5ad1d2890e4d9791def3b61d1c
kramstyles <mark.eke@decagon.dev>
2022-03-29 20:28:40 +0100

    added email field and save function

--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import RegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f"User: {username} registered")
+            form.save()
             return redirect('blog_home')
         else:
             messages.warning(request, 'Wrong Form Info')
-    else: form = UserCreationForm()
+    else: form = RegisterForm()
     context = {
         'form': form,
         'title': 'Register Users'
