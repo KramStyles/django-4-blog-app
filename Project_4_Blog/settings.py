318a1c38be258fa9bf2ff7096198cb642beb5212
kramstyles <mark.eke@decagon.dev>
2022-03-29 18:50:45 +0100

    created users app

--- Project_4_Blog/settings.py
+++ Project_4_Blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog_app.apps.BlogAppConfig'
+    'blog_app.apps.BlogAppConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
