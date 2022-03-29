606abbd257e27ceba898facc58fb3019cf4f6e8d
kramstyles <mark.eke@decagon.dev>
2022-03-29 16:44:05 +0100

    added and got blog post from db

--- blog_app/models.py
+++ blog_app/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     body = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
