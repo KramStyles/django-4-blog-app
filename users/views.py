0d0004cd64a0aa9406c8fdb4e51a084c220723f5
kramstyles <mark.eke@decagon.dev>
2022-03-29 20:09:05 +0100

    validated form

--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"User: {username} registered")
+            return redirect('blog_home')
+        else:
+            messages.warning(request, 'Wrong Form Info')
+    else: form = UserCreationForm()
     context = {
-        'form': UserCreationForm()
+        'form': form,
+        'title': 'Register Users'
     }
     return render(request, 'users/register.html', context)
