cefd191dd37ec58f04a7f9366f120de1ccf0ac73
kramstyles <mark.eke@decagon.dev>
2022-03-30 21:16:40 +0100

    used class views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog_home'),
+    path('', views.HomeView.as_view(), name='blog_home'),
     path('about/', views.about, name='blog_about'),
     path('post/', views.post, name='blog_post'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,15 +1,34 @@
-from turtle import pos
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-def index(request):
-    post = Post.objects.all()
-    context = {
-        'title': 'home',
-        'blogs': post
-    }
-    return render(request, 'blog/index.html', context)
+
+# def index(request):
+#     posts = Post.objects.all()
+#     context = {
+#         'title': 'home',
+#         'blogs': posts
+#     }
+#     return render(request, 'blog/index.html', context)
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'blog_posts'
+    ordering = ['-date_posted']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'home'
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    context_object_name = 'blog'
 
 
 def about(request):
